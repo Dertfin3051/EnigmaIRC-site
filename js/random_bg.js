ab71fa2d57111613fa19488092adf832e10da9c6
dertfin1 <Dertfin@gmail.com>
2024-03-23 22:30:31 +0300

    optimisation

--- js/random_bg.js
+++ js/random_bg.js
@@ -3,5 +3,5 @@ const container_1 = document.getElementById("container-1");
 const num = Math.random();
 
 if (num > 0.75) {
-    container_1.style.backgroundImage = "url('../images/bg_windows.png')";
+    container_1.style.backgroundImage = "url('../images/bg_windows.jpg')";
 }
