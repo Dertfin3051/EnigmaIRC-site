18063d8298a1730ed682e170b15d2cafb3bf9dc7
dertfin <dertfin@gmail.com>
2024-04-04 22:33:09 +0300

    04.04.24_4

--- js/download.js
+++ js/download.js
@@ -1,6 +1,6 @@
-const windows = "";
-const linux = "";
-const server = "";
+const windows = "https://github.com/Dertfin3051/EnigmaIRC/releases/download/v1.x/EnigmaIRC_Windows_v1-0beta.zip";
+const linux = "https://github.com/Dertfin3051/EnigmaIRC/releases/download/v1.x/EnigmaIRC_Client_Linux_v1-0beta.tar.gz";
+const server = "https://github.com/Dertfin3051/EnigmaIRC/releases/download/v1.x/EnigmaIRC_Server_Linux_v1-0beta.tar.gz";
 
 function dWindows() {window.open(windows)}
 function dLinux() {window.open(Linux)}
